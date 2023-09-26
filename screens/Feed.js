4d170ff1547344b13439c4f9522a3f33fcf63824
ShreyBafna1309 <128043864+ShreyBafna1309@users.noreply.github.com>
2023-09-26 21:07:24 +0530

    Add files via upload

---
+++ screens/Feed.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class FeedScreen extends React.Component{
+  render(){
+    return(
+      <View> 
+        <Text style={{color: "black", fontSize: 20}}> Feed Screen </Text>
+      </View>
+    )
+  }
+}
---
+++ screens/Profile.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class ProfileScreen extends React.Component{
+  render(){
+    return(
+      <View> 
+        <Text style={{color: "black", fontSize: 20}}> Profile Screen </Text>
+      </View>
+    )
+  }
+}
