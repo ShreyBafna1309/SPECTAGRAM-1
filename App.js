01778b48ba91553a5059a8ef2850692e945c706c
ShreyBafna1309 <128043864+ShreyBafna1309@users.noreply.github.com>
2023-09-26 21:10:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class App extends React.Component{
+  
+}
---
+++ screens/CreatePost.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class CreatePost extends React.Component{
+  render(){
+    return(
+      <View> 
+        <Text style={{color: "black", fontSize: 20}}> Create Post </Text>
+      </View>
+    )
+  }
+}
